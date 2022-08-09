379e7e352dc349c8aa16096137b263163ce2f82a
ramangkaur <110474162+ramangkaur@users.noreply.github.com>
2022-08-09 17:26:59 +0530

    Delete GraphQL-example directory
    deleting these files

--- GraphQL-example/src/main/java/com/techprimers/graphql/springbootgraphqlexample/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.techprimers.graphql.springbootgraphqlexample.repository;
-
-import com.techprimers.graphql.springbootgraphqlexample.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, String> {
-}
